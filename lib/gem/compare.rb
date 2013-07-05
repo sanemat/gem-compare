4045b065065c26dc387f952313b11cea0daae9b8
sanemat <o.gata.ken@gmail.com>
2013-07-05 18:02:57 +0900

    Inital commit

---
+++ lib/gem/compare.rb
@@ -0,0 +1,7 @@
+require "gem/compare/version"
+
+module Gem
+  module Compare
+    # Your code goes here...
+  end
+end
---
+++ lib/gem/compare/version.rb
@@ -0,0 +1,5 @@
+module Gem
+  module Compare
+    VERSION = "0.0.1"
+  end
+end
